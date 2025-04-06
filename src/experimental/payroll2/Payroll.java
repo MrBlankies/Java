c8e1ad6acdddf2234102a4a3f481262c65c65264
MrBlankies <alexgaouis@gmail.com>
2025-04-06 21:38:01 +0300

    Ignore experimental package

--- src/experimental/payroll2/Payroll.java
+++
@@ -1,9 +0,0 @@
-package experimental.payroll2;
-
-public class Payroll {
-    public static void main(String[] args) {
-        EmployeeList employees = new EmployeeList();
-        employees.loadEmployees();
-        employees.printPayroll();
-    }
-}
